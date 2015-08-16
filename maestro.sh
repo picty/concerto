81e885758670245522abacb00da6818d875058f3
Olivier Levillain <olivier.levillain@picty.org>
2015-08-16 19:35:31 +0200

    Rewrite maestro.sh to allow for multiple dumps and CAs.

--- maestro.sh
+++ maestro.sh
@@ -1,16 +1,54 @@
 #!/bin/bash
 
-BIN_DIR="$(dirname "$0")"
-
-DATA_DIR="$1"
-ANSWER_DUMP="$2"
-shift 2
-
 error () {
       echo "Error: $1" >&2
-      echo "Usage: maestro OUT_DIR ANSWER_DUMP TRUSTED_CA"
+      echo "Usage: maestro -d OUT_DIR [-D TRUSTED_CA_DIR|-C TRUSTED_CA] ANSWER_DUMPS"
       exit 1
 }
+
+
+BIN_DIR="$(dirname "$0")"
+DATA_DIR=
+TRUSTED_CAS=
+VERBOSE=
+
+while getopts "d:C:D:v" option; do
+    case "$option" in
+        d)
+            DATA_DIR="$OPTARG"
+            ;;
+        D)
+            TRUSTED_CAS="$(ls -1 "$OPTARG"/*) $TRUSTED_CAS"
+            ;;
+        C)
+            TRUSTED_CAS="$OPTARG $TRUSTED_CAS"
+            ;;
+        v)
+            VERBOSE=1
+            ;;
+        *)
+            error "Invalid option \"$option\""
+            ;;
+    esac
+done
+
+[ -d "$DATA_DIR" ] || mkdir "$DATA_DIR" &> /dev/null || error "Unable to create \"$DATA_DIR\" output directory."
+
+shift $(($OPTIND - 1))
+
+
+
+if [ -n "$VERBOSE" ]; then
+    echo "DATA_DIR=$DATA_DIR"
+    echo -n "TRUSTED_CAS="
+    for i in $TRUSTED_CAS; do echo -n "$i "; done
+    echo
+    echo -n "ANSWER_DUMPS ($#)="
+    for i in "$@"; do echo -n "$i "; done
+    echo    
+fi
+
+
 
 handle_ret_code () {
     if [ "$?" = "0" ]; then
@@ -22,14 +60,17 @@ handle_ret_code () {
     fi
 }
 
-[ -d "$DATA_DIR" ] || mkdir "$DATA_DIR" &> /dev/null || error "Unable to create \"$DATA_DIR\" output directory."
-[ -f "$ANSWER_DUMP" ] || error "Answer dump \"$ANSWER_DUMP\" does not seem to be a regular file."
-# which piatto > /dev/null || error "piatto executable can not be found."
 
 
 echo "= Extracting answers ="
-time "$BIN_DIR/injectAnswerDump" -d "$DATA_DIR" "$ANSWER_DUMP" 2> /dev/null
+time "$BIN_DIR/injectAnswerDump" -d "$DATA_DIR" "$@" 2> /dev/null
 handle_ret_code
+
+if [ "$TRUSTED_CAS" != "" ]; then
+    echo "= Injecting certs ="
+    "$BIN_DIR/inject" -d "$DATA_DIR" -t certs $TRUSTED_CAS
+    handle_ret_code
+fi
 
 echo "= Parsing certs ="
 time "$BIN_DIR/parseCerts" -d "$DATA_DIR" $( for i in $(seq 0 255); do printf "%2.2x " $i; done ) 2> /dev/null
@@ -48,12 +89,12 @@ echo "= Building chains ="
 time "$BIN_DIR/buildChains" -d "$DATA_DIR" 2> /dev/null
 handle_ret_code
 
-if [ "$*" = "" ]; then
+if [ "$TRUSTED_CAS" = "" ]; then
     touch "$DATA_DIR/trusted_certs.csv" "$DATA_DIR/trusted_chains.csv"
     touch "$DATA_DIR/rated_chains.csv"
 else
     echo "= Flaging trusted certs "
-    time "$BIN_DIR/flagTrust" -d "$DATA_DIR" --der "$@" 2> /dev/null
+    time "$BIN_DIR/flagTrust" -d "$DATA_DIR" --der $TRUSTED_CAS 2> /dev/null
     handle_ret_code
 
     echo "= Rating chains ="
