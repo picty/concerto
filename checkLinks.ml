7ce561fc73df981167d25ac0f67b4b879e195440
Olivier Levillain <olivier.levillain@picty.org>
2015-08-29 23:33:22 +0200

    New option in checkLinks: suffix.
    Using both -M option in prepareLinks and this suffix option, it is now trivial
    to parallelize the checkLinks step!

--- checkLinks.ml
+++ checkLinks.ml
@@ -14,19 +14,22 @@ open FileOps
 open X509Util
 
 let data_dir = ref ""
+let suffix = ref ""
 
 let options = [
   mkopt (Some 'h') "help" Usage "show this help";
   mkopt (Some 'd') "data-dir" (StringVal data_dir) "set the data directory";
+
+  mkopt None "suffix" (StringVal suffix) "adds a suffix to the CSV files used";
 ]
 
 
-let check_one_line ops = function
+let check_one_line ops links_filename = function
   | [subject_h; issuer_h] ->
      let s = sc_of_raw_value subject_h false (ops.read_file "certs" subject_h)
      and i = sc_of_raw_value issuer_h false (ops.read_file "certs" issuer_h) in
      if check_link_bool (cert_of_sc i) (cert_of_sc s)
-     then ops.write_line "links" "" [subject_h; issuer_h]
+     then ops.write_line links_filename "" [subject_h; issuer_h]
   | _ -> raise (InvalidNumberOfFields 2)
 
 
@@ -36,7 +39,7 @@ let _ =
   if !data_dir = "" then usage "checkLinks" options (Some "Please provide a valid data directory");
   try
     let ops = prepare_data_dir !data_dir in
-    ops.iter_lines "possible_links" (check_one_line ops);
+    ops.iter_lines ("possible_links" ^ !suffix) (check_one_line ops ("links" ^ !suffix));
     ops.close_all_files ()
   with
     | ParsingException (e, h) -> prerr_endline (string_of_exception e h); exit 1
